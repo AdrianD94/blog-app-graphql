ab6054a0f92094351f34e8c80cd5273a46b23bcd
adyy <ady.drozman2007@gmail.com>
2021-12-21 21:24:19 +0200

    added me query

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -2,6 +2,14 @@ import { Post } from ".prisma/client";
 import { Context } from "..";
 
 export const Query = {
+  me: async (_: any, __: any, { prisma, userInfo }: Context) => {
+    if (!userInfo) return null;
+    return prisma.user.findUnique({
+      where: {
+        id: userInfo.userId,
+      },
+    });
+  },
   posts: async (_: any, __: any, { prisma }: Context) => {
     const posts: Post[] = await prisma.post.findMany({
       orderBy: [
